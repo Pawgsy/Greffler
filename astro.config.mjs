2d415da21a7666ea15e665cf43eb0e76d9b38821
Florian Koch <floko888@gmail.com>
2025-04-20 10:52:21 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
     site:"https://greffler.de",
     outDir:"./docs",
-    cacheDir:"/buildcache"
+    cacheDir:"./buildcache"
 });
